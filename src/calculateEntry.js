108652a96ae44f0101396aaca5c074e156dc24d1
Bruna Santiago <brunasanthiago@outlook.com>
2022-04-19 15:56:16 -0300

    80%, ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,31 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter(
+    (entrant) => entrant.age >= 18 && entrant.age < 50,
+  ).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  const age = countEntrants(entrants);
+  if (age !== 0) {
+    const priceChild = age.child * data.prices.child;
+    const priceAdult = age.adult * data.prices.adult;
+    const priceSenior = age.senior * data.prices.senior;
+    return priceChild + priceAdult + priceSenior;
+  }
+  return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,30 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function completa(opt, obj) {
+  const nome = species.reduce((acc, curr) => {
+    const residente = opt.sex
+      ? curr.residents.filter((res) => res.sex === opt.sex)
+      : curr.residents;
+    const names = residente.map((res) => res.name);
+    acc[curr.location].push({ [curr.name]: opt.sorted ? names.sort() : names });
+    return acc;
+  }, obj);
+  return nome;
+}
 
 function getAnimalMap(options) {
   // seu código aqui
+  const obj = { NE: [], NW: [], SE: [], SW: [] };
+  if (!options || Object.values(options)[0] === 'female') {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.location] = [...acc[specie.location], specie.name];
+      return acc;
+    }, obj);
+  }
+  if (options.includeNames) {
+    return completa(options, obj);
+  }
 }
 
 module.exports = getAnimalMap;
